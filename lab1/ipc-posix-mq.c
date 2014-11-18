06341bc5f9e448c5d011588b65571eb79416cb97
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-11-18 14:14:12 +0100

    Error handling in ipc-posix-queue

--- lab1/ipc-posix-mq.c
+++ lab1/ipc-posix-mq.c
@@ -10,26 +10,31 @@
 
 #define MAX_BUFFER 1024
 #define SAMPLE_TEXT "YOLO!"
-#define QNAME "/yummy_quuee"
+#define QNAME "/yummypipe"
+
+#define err_check(status, msg)\
+	if(!(status))\
+		fprintf(stderr, "Error %s. Error code : %d \n", msg, errno);\
+
 
 main(){
 	pid_t childid;
 	int childec;
 	mqd_t mqd;
 	struct mq_attr attr;
-	ssize_t bytes_read = -1;
+	ssize_t bytes_read;
 	char buffer[MAX_BUFFER + 1];
 	
 	// Set attributes
 	attr.mq_flags = 0;  
 	attr.mq_maxmsg = 10;  
-	attr.mq_msgsize = 33;  
+	attr.mq_msgsize = MAX_BUFFER;  
 	attr.mq_curmsgs = 0;  
 
 	// Create a queue
-	mqd = mq_open(QNAME, O_CREAT | O_RDONLY, 0644, &attr);
-
-	printf("mqd is : %d \n", mqd);
+	mqd = mq_open(QNAME, O_CREAT, 0644, &attr);
+	err_check(0 <= mqd, "creating queue");
+	
 	// Attempt to fork
 	childid = fork();
 	
@@ -39,16 +44,13 @@ main(){
 		
 		// Open queue with READ access
 		mqd = mq_open(QNAME, O_RDONLY);
-
-		printf("mqdp is : %d \n", mqd);		
-
+		err_check(0 <= mqd, "opening queue");
+		
 		// Read from queue
-		bytes_read = mq_receive(mqd, buffer, MAX_BUFFER, NULL);
-		if(bytes_read == -1)
-			printf("Errno is : %d\n", errno);		
+		err_check(0 <= mq_receive(mqd, buffer, MAX_BUFFER, NULL), "reading queue");	
 
 		// Print the read data
-		printf("Data received : %d, %s \n", (int) bytes_read, buffer);
+		printf("Data received : %s \n", buffer);
 
 		// Wait for child
 		wait(&childec);
@@ -61,19 +63,16 @@ main(){
 		
 		// Open queue with WRITE access
 		mqd = mq_open(QNAME, O_WRONLY);
-		printf("mqdc is : %d \n", mqd);
+		err_check(0 <= mqd, "opening queue");
+		
 		// Write to queue
-		bytes_read = mq_send(mqd, SAMPLE_TEXT, sizeof(SAMPLE_TEXT), 1);
-		printf("Send status: %d \n", bytes_read);
+		err_check(0 <= mq_send(mqd, SAMPLE_TEXT, sizeof(SAMPLE_TEXT), 1), "writing queue");
 	}
 
 	// Fork failed so, terminate
 	else{
-		printf("Failed to fork. Error! \n");
-		perror("fork");
-		exit(0);
+		printf("Failed to fork. Error code : %d \n", errno);
 	}
 
 }
-
 
