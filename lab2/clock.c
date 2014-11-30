028ce7999df32210d8a8d7f8c231873d055b2aff
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-11-30 22:23:37 +0100

    Clock and procfile added to clock module

--- lab2/clock.c
+++ lab2/clock.c
@@ -1,6 +1,14 @@
-#include <linux/module.h>
-#include <linux/errno.h>
-#include <linux/fs.h>
+#include <linux/module.h>	/* Specifically, a module */
+#include <linux/kernel.h>	/* We're doing kernel work */
+#include <linux/proc_fs.h>	/* Necessary because we use the proc fs */
+#include <asm/uaccess.h>	/* for copy_from_user */
+#include <linux/errno.h>	/* Error numbers and errno */
+#include <linux/time.h>		/* for kernel time functions */
+#include <linux/slab.h>		/* For kmalloc */
+
+#define CDEV_FILE_NAME "clock"
+#define CDEV_MAJOR 2
+#define CDEV_MINOR 3
 
 MODULE_AUTHOR("Praveen Kumar Pendyala");
 MODULE_DESCRIPTION("Lab 2 Solution Clock driver");
@@ -10,7 +18,16 @@ MODULE_LICENSE("GPL");
 static ssize_t clock_read(struct file *file, char *buf, size_t count,
 						loff_t *ppos)
 {
-
+	struct timeval *tv;
+	tv = kmalloc(sizeof(*tv), GFP_KERNEL);
+	
+	// Get current time
+	do_gettimeofday(tv);
+	
+	printk("The time is: %ld\n", tv->tv_sec);
+	
+	// Format time
+	return 0;
 }
 
 // Write clock call - unsupported
@@ -18,22 +35,25 @@ static ssize_t clock_write( struct file *file, const char *buf, size_t count,
 						 loff_t *ppos )			
 {
 	printk("Operation not supported");
+	return -EPERM;
 }
 
 // Open device call
 static int clock_open(struct inode * inode, struct file * file)
 {
-
+	return 0;	
 }
+
 
 // Release clock for future operations
 static int clock_release(struct inode * inode, struct file * file)
 {
-
+	return 0;
 }
 
 // clocks' file operations structure
-static struct file_operations gen_module_fops = {
+
+static struct file_operations clock_fops = {
 	.owner =	THIS_MODULE,
 	.read =		clock_read,
 	.write =	clock_write,
@@ -43,14 +63,15 @@ static struct file_operations gen_module_fops = {
 
 // Initialize clock device on insmod
 static int __init clock_device_init(void)
-{
-
+{	
+	proc_create(CDEV_FILE_NAME, 0, NULL, &clock_fops);
+	return 0;
 }
 
 // Remove clock device on rmmod
 static void __exit clock_device_cleanup(void)
 {
-
+	remove_proc_entry(CDEV_FILE_NAME, NULL);
 }
 
 module_init(clock_device_init);
