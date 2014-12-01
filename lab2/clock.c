293baad1fe475dee4416f53e7a2cd11d5ccd0f0c
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-12-01 02:07:00 +0100

    Completed epoch - datetime conversion

--- lab2/clock.c
+++ lab2/clock.c
@@ -7,8 +7,11 @@
 #include <linux/slab.h>		/* For kmalloc */
 
 #define CDEV_FILE_NAME "clock"
-#define CDEV_MAJOR 2
-#define CDEV_MINOR 3
+#define YEAR_LEN 4
+#define MON_DAY_LEN 2
+#define SEP_LEN 1
+#define HR_MIN_SEC_LEN 2 
+#define TS_LEN (YEAR_LEN + 2*MON_DAY_LEN + 3*HR_MIN_SEC_LEN + 5*SEP_LEN + 4)
 
 MODULE_AUTHOR("Praveen Kumar Pendyala");
 MODULE_DESCRIPTION("Lab 2 Solution Clock driver");
@@ -17,35 +20,90 @@ MODULE_LICENSE("GPL");
 // To trigger end of file while reading
 int eof = 0;
 
+/*********************************************
+ For Epoch to datetime conversion
+ Conversion routine credits: Stack overflow
+********************************************/
+struct date_time_t {
+	int second;
+	int minute;
+	int hour;
+	int day;
+	int month;
+	int year;
+};
+
+static unsigned short days[4][12] =
+	{
+	    {   0,  31,  60,  91, 121, 152, 182, 213, 244, 274, 305, 335},
+	    { 366, 397, 425, 456, 486, 517, 547, 578, 609, 639, 670, 700},
+	    { 731, 762, 790, 821, 851, 882, 912, 943, 974,1004,1035,1065},
+	    {1096,1127,1155,1186,1216,1247,1277,1308,1339,1369,1400,1430},
+	};
+
+// Epoch to datetime conversion
+static void epoch_to_date_time(struct date_time_t* date_time, unsigned int epoch)
+{
+	date_time->second = epoch%60; epoch /= 60;
+	date_time->minute = epoch%60; epoch /= 60;
+	date_time->hour   = epoch%24; epoch /= 24;
+
+	unsigned int years = epoch/(365*4+1)*4; epoch %= 365*4+1;
+
+	unsigned int year;
+	for (year=3; year>0; year--)
+	{
+		if (epoch >= days[year][0])
+			break;
+	}
+
+	unsigned int month;
+	for (month=11; month>0; month--){
+		if (epoch >= days[year][month])
+			break;
+	}
+
+	date_time->year  = years+year;
+	date_time->month = month+1;
+	date_time->day   = epoch-days[year][month]+1;
+}
+
+/**********************************************
+ End of Epoch to datetime conversion
+**********************************************/
+
 // Read clock call
 static ssize_t clock_read(struct file *file, char *user_buf, size_t count,
 						loff_t *ppos)
 {
 	struct timeval *tv;
-	char *time;
+	struct date_time_t *dt;
+	char *ts;
 	
 	// Signal eof if previous read was done.
 	if(eof == 1)
 		return 0;
 	
 	tv = kmalloc(sizeof(*tv), GFP_KERNEL);
-	time = kmalloc(sizeof(*time) * 50, GFP_KERNEL);
+	dt = kmalloc(sizeof(*dt), GFP_KERNEL);
+	ts = kmalloc(sizeof(*ts) * TS_LEN, GFP_KERNEL);
 	
 	// Get current time
 	do_gettimeofday(tv);
-	sprintf(time, "time:%ld\n", tv->tv_sec);
+	epoch_to_date_time(dt, tv->tv_sec);
 	
-	// Debug time in kernel log
-	printk("The time is: %ld\n", tv->tv_sec);
+	// Format the time. +1 for hours is to get to EST time zone
+	sprintf(ts, "%d-%d-%d %02d:%02d:%02d\n\0", dt->day, dt->month, 
+		dt->year + 1970, dt->hour + 1, dt->minute, dt->second);
 	
 	// Copy to userspace
-	copy_to_user(user_buf, time, 50);
+	copy_to_user(user_buf, ts, TS_LEN);
 	
 	// Set eof to 1 for next read's
 	eof = 1;
 	
 	// Format time
-	return 50;
+	return TS_LEN;
 }
 
 // Write clock call - unsupported
