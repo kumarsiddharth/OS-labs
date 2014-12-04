a45d4afa6756f80f5ee072079be0bc5696ceff3c
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-12-04 15:37:13 +0100

    Updated permissions while creating FIFOs

--- lab2/mount_fifos.sh
+++ lab2/mount_fifos.sh
@@ -1,5 +1,5 @@
-sudo mknod /dev/fifo0 c 240 0
-sudo mknod /dev/fifo1 c 240 1
-sudo mknod /dev/fifo2 c 240 2
-sudo mknod /dev/fifo3 c 240 3
+sudo mknod -m a+rwx /dev/fifo0 c 240 0
+sudo mknod -m a+rwx /dev/fifo1 c 240 1
+sudo mknod -m a+rwx /dev/fifo2 c 240 2
+sudo mknod -m a+rwx /dev/fifo3 c 240 3
 
